17c3d6697ea77451b99ec80ce651258847420d2f
fox-tomoki-tabei <tomoki.tabei@fox-hound.jp>
2017-01-28 09:35:27 +0900

    画像とか色々追加

--- index.php
+++ index.php
@@ -1,2 +1,33 @@
-<?php echo 'hackathon!!';
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html" charset="utf-8">
+      <link rel="stylesheet" href="/css/style.css">
 
+      <!-- <script src="http://52.193.125.145:3000/socket.io/socket.io.js" charset="utf-8"></script>
+      <script src="http://52.193.125.145:3000/www/js/vehicleAPI.js" charset="utf-8"></script>
+      <script src="http://52.193.125.145:3000/www/js/vehicleAPI-client.js" charset="utf-8"></script> -->
+
+      <!-- jquery-3.1.1 -->
+      <script src="js/jquery-3.1.1.js" charset="utf-8"></script>
+      <!-- app.js -->
+      <script src="js/app.js" charset="utf-8"></script>
+      <!-- MAP -->
+      <script src="http://support.chizumaru.com/aspusers/online_manual/AjaxFramework/3.3.13/lib/Mapple.js" type="text/javascript"></script>
+	    <script src="http://support.chizumaru.com/aspusers/online_manual/AjaxFramework/3.3.13/lib/slider.js" type="text/javascript"></script>
+	    <script src="http://support.chizumaru.com/aspusers/online_manual/AjaxFramework/3.3.13/lib/scrollmap.js" type="text/javascript"></script>
+
+    <title>村おこし</title>
+  </head>
+  <body>
+    <div id="map"></div>
+    <div id="">
+      Room ID:<input type="text" id="WSRoomID"></input><button id="connect">START</button>
+    </div>
+    <div id=""></div>
+    <div id=""></div>
+    <div id=""></div>
+    <div id=""></div>
+    <div id=""></div>
+  </body>
+</html>
