b61324a903e0a13fb16e53aaff8d9b837336ea1a
KosukeWatanabe <kosuke.watanabe@fox-hound.jp>
2017-01-28 16:36:14 +0900

    room.js 処理修正

--- room.js
+++ room.js
@@ -1,7 +1,7 @@
-var roomID = "YOUR.NAME.HERE@example.com"; //ユニークな文字列に変更して下さい
+var roomID = "team-h"; //ユニークな文字列に変更して下さい
 
 window.onload = function () {
-  document.getElementById("WSRoomID").innerHTML = roomID;
+  // document.getElementById("WSRoomID").innerHTML = roomID;
   var msg = {"roomID":roomID, "data":"NOT REQUIRED"};
   socket.emit('joinRoom', JSON.stringify(msg));
 };
