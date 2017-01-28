46c71beb4d3d08f4c4b87ef829cede8f79a6e084
KosukeWatanabe <kosuke.watanabe@fox-hound.jp>
2017-01-28 13:48:58 +0900

    移動位置調整,画像加工

--- move.js
+++ move.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  for (var i = 1; i < 260; i++) {
+  for (var i = 1; i < 500; i++) {
     var el = document.createElement('div');
     el.className = "sprite";
     el.id = i;
@@ -18,17 +18,17 @@ $(document).ready(function() {
   function move(id){
     var self = this;
     init_rand_left = Math.floor(Math.random() * 1400);
-    init_rand_top = Math.floor(Math.random() * 700);
-    $('#'+ id).animate({'top':init_rand_top,'left':init_rand_left}, 3000);
+    init_rand_bottom = Math.floor(Math.random() * 300);
+    $('#'+ id).animate({'bottom':init_rand_bottom,'left':init_rand_left}, 3000);
     setInterval(function(){
       var self = this;
       var rand_left = Math.floor( Math.random() * 1400 ) ;
-      var rand_top = Math.floor( Math.random() * 700 ) ;
+      var rand_bottom = Math.floor( Math.random() * 300 ) ;
 
       if($('#' + id).hasClass('reflect')) {
-        $('#'+ id).removeClass('reflect').animate({'top': rand_top,'left':rand_left},3000);
+        $('#'+ id).removeClass('reflect').animate({'bottom': rand_bottom,'left':rand_left},3000);
       }else{
-        $('#'+ id).addClass('reflect').animate({'top':rand_top,'left':rand_left}, 3000);
+        $('#'+ id).addClass('reflect').animate({'bottom':rand_bottom,'left':rand_left}, 3000);
       }
     }, Math.floor(Math.random() * 4000) + 500);
   }
